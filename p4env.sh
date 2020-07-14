458fd465f46e1a9a265f3c4908e8709e3806f5b0
Victor Hugo <53232054+VHchavez@users.noreply.github.com>
2020-07-14 12:18:35 -0400

    Update p4env.sh

--- p4env.sh
+++ p4env.sh
@@ -1,7 +1,7 @@
 conda create -y -n p4env python=3.7
 conda activate p4env
+conda install -y -c conda-forge jupyterlab matplotlib qcelemental qcportal
 conda install -y -c psi4 psi4
-conda install -y -c conda-forge jupyterlab matplotlib
 conda install -y -c plotly plotly
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
@@ -10,4 +10,6 @@ nvm install node
 nvm ls
 
 jupyter labextension install @ijmbarr/jupyterlab_spellchecker
+jupyter labextension install jupyterlab-plotly@4.8.2
+jupyter labextension install @jupyter-widgets/jupyterlab-manager plotlywidget@4.8.2
 
