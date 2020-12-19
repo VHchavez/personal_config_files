c441052be3c64434352a6dbeebe74affd5dda389
Victor Hugo <53232054+VHchavez@users.noreply.github.com>
2020-12-19 16:18:44 -0500

    Update p4env.sh

--- p4env.sh
+++ p4env.sh
@@ -1,7 +1,7 @@
-conda create -y -n p4env python=3.7
+conda create -y -n p4env python=3.8
 conda activate p4env
 conda install -y -c conda-forge jupyterlab matplotlib qcelemental qcportal
-conda install -y -c psi4 psi4
+conda install -y -c psi4/label/dev psi4
 conda install -y -c plotly plotly
 
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
